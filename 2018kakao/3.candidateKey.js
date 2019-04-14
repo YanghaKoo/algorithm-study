1a4e693b831c20600d133e58ec207bb9ff800c7a
koo <yangha93@naver.com>
2019-04-14 22:40:44 +0900

    0414

--- 2018kakao/3.candidateKey.js
+++ 2018kakao/3.candidateKey.js
@@ -1,22 +1,41 @@
 // 카카오 3번 후보키 문제
 
 function solution(relation) {
+  function hasDuplicates(array) {
+    return new Set(array).size !== array.length;
+  }
+
   
-  
-  
-  
-   
-  
-  
+  const rLength = relation.length; // 6
+  const cLength = relation[0].length; // 4
+
+  const newArr = new Array(cLength).fill(0).map(v=>[]);
+
+
+  for (let i = 0; i < rLength; i++) {
+    for (let j = 0; j < cLength; j++) {
+      
+      newArr[j].push(relation[i][j]);
+      console.log(relation[i][j])
+      
+    }
+  }
+
+  console.log(newArr);
+
+  return 'end'
 }
 
-console.log(solution([
-  ["100", "ryan", "music", "2"],
-  ["200", "apeach", "math", "2"],
-  ["300", "tube", "computer", "3"],
-  ["400", "con", "computer", "4"],
-  ["500", "muzi", "music", "3"],
-  ["600", "apeach", "music", "2"]
-]));         // 2
- 
 
+
+// 최대길이 20 row
+console.log(
+  solution([
+    ["100", "ryan", "music", "2"],
+    ["200", "apeach", "math", "2"],
+    ["300", "tube", "computer", "3"],
+    ["400", "con", "computer", "4"],
+    ["500", "muzi", "music", "3"],
+    ["600", "apeach", "music", "2"]
+  ])
+); // 2
