b07b70b5cc60c0519d66c77b92be6747111beabc
koo <yangha93@naver.com>
2019-05-20 01:15:09 +0900

    0520

--- 2019_11st/2.py
+++ 2019_11st/2.py
@@ -14,7 +14,7 @@
 # 2. 나온 문자가 case 1인 경우, 지금까지 저장된 number을 string to number로 type casting 다시 1을 반복
 # 3. 나온 문자가 case 2인 경우 지금까지의 문자열을 number만큼 반복
 # 4. 나온 문자가 case 3인 경우 지금까지의 문자열에 concat
-# 5. 나온 문자가 case 4인 경우 number을 일단 임시 저장
+# 5. 나온 문자가 case 4인 경우 number을 일단 임시 저 장
 
 
 class Stack:
