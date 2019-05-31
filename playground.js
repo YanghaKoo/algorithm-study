c7a9a75c1ccfb6a391911b3c09e8702d034140dc
koo <yangha93@naver.com>
2019-06-01 00:08:44 +0900

    0601

--- playground.js
+++ playground.js
@@ -1,5 +1,39 @@
-function test(string){
- return +string
+function solution(string){
+  let answer = 0
+  let length = string.length // 4
+
+  for(let i=0; i<string.length; i++){
+    answer += Number(string[i]) * Math.pow(2,length - i -1)
+  }
+  return answer
 }
 
-console.log(typeof test("1234")) 
+console.log(solution("1011")) // r
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const readline=require("readline");
+ 
+// const rl =readline.createInterface({
+//   input:process.stdin,
+//   output:process.stdout
+// });
+ 
+// rl.setPrompt("## ");
+ 
+// rl.prompt();
+// rl.on("line",(data)=>{
+//   console.log(data);
+//   rl.prompt();
+// });
+ 
